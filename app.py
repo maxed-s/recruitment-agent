13d0d178cc253ceb8f55d6dcaf8b4964ece853f7
Shlok Sharma <shlokzartsharma@gmail.com>
2023-07-19 17:20:13 -0500

    uses griptape; hardcoded recruitment agent

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import openai
 import os
-from langchain.llms import OpenAI
 
 env_file = os.path.join(os.path.dirname(__file__), ".env")
 if os.path.isfile(env_file):
@@ -11,12 +10,35 @@ if os.path.isfile(env_file):
       key, value = line.split("=", 1)
       os.environ[key] = value
 
-my_openai_key = os.environ["OPENAI_API_KEY"]
+openai_api_key = os.environ["OPENAI_API_KEY"]
 
-llm = OpenAI(openai_api_key=my_openai_key)
+# ABOVE DESCRIBES IMPORT OF OPENAI KEY
 
-llm = OpenAI(temperature=0.9)
 
-if __name__ == "__main__":
-  print(llm.predict("What would be a good company name for a company that makes colorful socks?"))
+from griptape import utils
+from griptape.structures import Agent
+from griptape.rules import Rule, Ruleset
+
+agent = Agent(
+    rulesets=[
+        Ruleset(
+            name="Recruitment Manager",
+            rules=[
+                Rule("Behave like a recruiter"),
+                Rule("Act like you've been hired by a B2B startup called MaxedS, that sells high-tech solutions to enterprise companies"),
+                Rule("Come up with job descriptions based on what is provided")
+            ]
+        )
+    ]
+)
+
+agent.run("hi there!")
+agent.run("I am looking for a software engineer")
+agent.run("What sort of job posting should I write to post on LinkedIn to attract the candidates I want")
+
+print(
+    utils.Conversation(agent.memory)
+)
+
+
 
