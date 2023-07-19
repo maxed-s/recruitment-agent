81908c0c86467d5cb82f286bdf7bf0f3601bb2a4
Shlok Sharma <shlokzartsharma@gmail.com>
2023-07-18 22:55:12 -0500

    took out any use of dotenv; can use basic langchain llm

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-from dotenv import load_dotenv
-
-load_dotenv()
-
+import openai
 import os
+from langchain.llms import OpenAI
 
-openai_api_key = os.getenv('OPENAI_API_KEY')
+env_file = os.path.join(os.path.dirname(__file__), ".env")
+if os.path.isfile(env_file):
+    for line in open(env_file):
+      line = line.strip()
+      if not line or line.startswith("#"):
+        continue
+      key, value = line.split("=", 1)
+      os.environ[key] = value
 
-# First, let's load the language model we're going to use to control the agent.
-chat = ChatOpenAI(temperature=0)
+my_openai_key = os.environ["OPENAI_API_KEY"]
 
-# Next, let's load some tools to use. Note that the `llm-math` tool uses an LLM, so we need to pass that in.
-llm = OpenAI(temperature=0)
-tools = load_tools(["serpapi", "llm-math"], llm=llm)
+llm = OpenAI(openai_api_key=my_openai_key)
 
-# Finally, let's initialize an agent with the tools, the language model, and the type of agent we want to use.
-agent = initialize_agent(tools, chat, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+llm = OpenAI(temperature=0.9)
 
-# Now let's test it out!
-agent.run("Who is Olivia Wilde's boyfriend? What is his current age raised to the 0.23 power?")
+if __name__ == "__main__":
+  print(llm.predict("What would be a good company name for a company that makes colorful socks?"))
+
